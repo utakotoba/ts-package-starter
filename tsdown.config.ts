aa7e2864f38d33fadafed456024a1b0cecc838f6
Yu <echo.aimless@gmail.com>
2026-06-18 13:46:05 +0800

    build: switch to workspace

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+  workspace: ['lib'],
   entry: 'src/index.ts',
   format: 'es',
   dts: {
     oxc: true,
     sourcemap: true,
   },
+  sourcemap: true,
   exports: true,
 })
